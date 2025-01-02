7168de99309e334b3dd54447999ecf38a7904dc3
Rhys1558 <rhyslewsley96@gmail.com>
2025-01-02 08:42:08 +0000

    I made a homepage for the blog site and i made a way for user to look at just one blog by its self also downloaded boots

--- questlog/questlogblog/urls.py
+++ questlog/questlogblog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 #from . import views
-from .views import HomeView
+from .views import HomeView, ArticleDetails
 
 urlpatterns = [
     #path("", views.home, name="home"),
     path("", HomeView.as_view(), name="home"),
+    path("article/<int:pk>", ArticleDetails.as_view(), name="article_details"),
 ]
+

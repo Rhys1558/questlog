922c983bc848c69aacd482fdad85ee73965d60ac
Rhys1558 <rhyslewsley96@gmail.com>
2025-01-07 13:14:09 +0000

    I made user authentication and a register, login and logout system

--- questlog/questlogblog/urls.py
+++ questlog/questlogblog/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-#from . import views
 from .views import HomeView, ArticleDetails, NewPost, EditPost, DeletePost
 
 urlpatterns = [
-    #path("", views.home, name="home"),
     path("", HomeView.as_view(), name="home"),
     path("article/<int:pk>", ArticleDetails.as_view(), name="article_details"),
     path("NewPost/", NewPost.as_view(), name="NewPost"),

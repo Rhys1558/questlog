71f8049a65b275ec17c0d5dac58122f72248e873
Rhys1558 <rhyslewsley96@gmail.com>
2025-01-07 10:23:03 +0000

    I made a date element to all blog posts and set todays date for all existing posts

--- questlog/questlogblog/views.py
+++ questlog/questlogblog/views.py
@@ -10,7 +10,7 @@ from django.urls import reverse_lazy
 class HomeView(ListView):
     model = Post 
     template_name = "home.html"
-    ordering = ["-id"]
+    ordering = ["-date_of_post"]
 
 class ArticleDetails(DetailView):
     model = Post

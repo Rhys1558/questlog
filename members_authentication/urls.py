e9f8f7324790aadd947ce5c01b53a66d791e653f
Rhys Lewsley <rhyslewsley96@gmail.com>
2025-01-16 05:24:48 +0000

    update navbar css styleing and made back button for register and log in pages

--- members_authentication/urls.py
+++ members_authentication/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
+from . import views
 from .views import UserRegister
 
 urlpatterns = [
     path("register/", UserRegister.as_view(), name="register"),
-
 ]
--- questlog/settings.py
+++ questlog/settings.py
@@ -136,12 +136,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # This points to the entire static directory
+    os.path.join(BASE_DIR, "static"),
 ]
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = BASE_DIR / "staticfiles" 
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
